4c83620cd1c63e30212cd19a505c983b91125077
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-20 23:49:03 +0500

    fix(Container): Fixed horizontal padding from container on xs size

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,4 +1,3 @@
-import { StyleProp, ViewStyle } from 'react-native';
 import { View, ViewProps } from '@bluebase/components';
 import { MAX_CONTAINER_WIDTH } from '../../constants';
 import React from 'react';
@@ -6,7 +5,6 @@ import { ScreenSizeObserver } from '../ScreenSizeObserver';
 import { Theme } from '@bluebase/core';
 
 export interface ContainerStyles {
-	paddingHorizontalXS: StyleProp<ViewStyle>;
 }
 
 export interface ContainerProps extends ViewProps {}
@@ -18,7 +16,6 @@ export const Container = ({ style, styles, ...rest }: ContainerProps & { styles:
 			{...rest}
 			style={[
 				{ width: MAX_CONTAINER_WIDTH[size], alignSelf: 'center' },
-				size === 'xs' && styles.paddingHorizontalXS,
 				style,
 			]}
 		/>
@@ -26,8 +23,5 @@ export const Container = ({ style, styles, ...rest }: ContainerProps & { styles:
 	</ScreenSizeObserver>
 );
 
-Container.defaultStyles = (theme: Theme) => ({
-	paddingHorizontalXS: {
-		paddingHorizontal: theme.spacing.unit * 2
-	}
+Container.defaultStyles = (_theme: Theme) => ({
 });

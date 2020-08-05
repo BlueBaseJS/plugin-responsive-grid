6753d94262b4135b710b0cafffa0cc28420d16c0
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-08-05 16:46:05 +0500

    refactor(Container)

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,27 +1,24 @@
 import { View, ViewProps } from '@bluebase/components';
+
 import { MAX_CONTAINER_WIDTH } from '../../constants';
 import React from 'react';
-import { ScreenSizeObserver } from '../ScreenSizeObserver';
 import { Theme } from '@bluebase/core';
+import { useScreenSize } from '../../hooks';
 
-export interface ContainerStyles {
-}
+export interface ContainerStyles {}
 
 export interface ContainerProps extends ViewProps {}
 
-export const Container = ({ style, styles, ...rest }: ContainerProps & { styles: ContainerStyles }) => (
-	<ScreenSizeObserver>
-	{(size) => (
-		<View
-			{...rest}
-			style={[
-				{ width: MAX_CONTAINER_WIDTH[size], alignSelf: 'center' },
-				style,
-			]}
-		/>
-	)}
-	</ScreenSizeObserver>
-);
+export const Container = ({
+	style,
+	styles,
+	...rest
+}: ContainerProps & { styles: ContainerStyles }) => {
+	const size = useScreenSize();
 
-Container.defaultStyles = (_theme: Theme) => ({
-});
+	return (
+		<View {...rest} style={[{ width: MAX_CONTAINER_WIDTH[size], alignSelf: 'center' }, style]} />
+	);
+};
+
+Container.defaultStyles = (_theme: Theme) => ({});

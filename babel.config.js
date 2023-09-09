b599789d44dd26627eef827f23ab6387d2d22de9
Abdul Rehman Talat <rehman.talat@gmail.com>
2023-09-09 17:06:22 +0500

    feat: Expo 49

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
-module.exports = {
-  "presets": [
-    "@bluebase/code-standards/babel.config"
-  ]
+module.exports = function(api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo', '@bluebase/code-standards/babel.config'],
+		plugins: ['react-native-reanimated/plugin'],
+		env: {
+			test: {
+				plugins: [
+					[
+						'istanbul',
+						{
+							exclude: ['**/*.test.{ts,tsx,js,jsx}', 'tests/*.{ts,tsx,js,jsx}'],
+						},
+					],
+				],
+			},
+		},
+	};
 };
---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../src/__stories__/Grid.stories');
+	require('../src/components/ResponsiveLayout/__stories__/Grid.stories');
+}
+
+const stories = [
+	'../src/__stories__/Grid.stories',
+	'../src/components/ResponsiveLayout/__stories__/Grid.stories',
+];
+
+module.exports = {
+	loadStories,
+	stories,
+};
